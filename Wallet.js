e01ccef74a6d023d512cc4ce7bb2364d1772594a
Jakub Gajewski <jacobs.hacking@gmail.com>
2019-06-10 18:18:32 +0100

    Update Wallet.js

--- Wallet.js
+++ Wallet.js
@@ -1,10 +1,10 @@
 class Wallet {
  constructor(money) {
   let _money = money;
-  //pobieranie aktualnej zawartości portfela
+  //Getting value of my wallet
   this.getWalletValue = () => _money;
 
-  //Sprawdzanie czy użytkonik ma odpowiednią ilość środków
+  //Checking of user has enought money
 
   this.checkCanPlay = value => {
    if (_money >= value) return true;
